7d95851c1672e22633f28b6ff5ecfc7809b4c692
specop0 <specop0@yahoo.de>
2015-12-10 12:43:20 +0100

    mehrere-ziele, patchday loadout and caching with addAction uses safe parse to prevent script errors

--- co40_op-patchday.Altis/spec_loadout/CfgFunctions.hpp
+++ co40_op-patchday.Altis/spec_loadout/CfgFunctions.hpp
@@ -2,9 +2,9 @@ class spec_loadout {
 	tag = "Spec";
 	class init {
 		file="spec_loadout";
+		class addContainer {};
+		class addItemToContainer {};
 		class loadout {};
 		class units_init { postInit = 1; };
-		class addItemToContainer {};
-		class addContainer {};
 	};
 };
