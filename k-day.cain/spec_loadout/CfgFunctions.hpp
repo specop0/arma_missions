f0ba9194f861029d362c81e32eca32fdf2fac7fe
specop0 <specop0@yahoo.de>
2016-11-13 11:11:36 +0100

    removed k-day because cain is no longer in mod repository

--- k-day.cain/spec_loadout/CfgFunctions.hpp
+++
@@ -1,9 +0,0 @@
-class spec_loadout {
-    tag = "Spec";
-    class init {
-        file="spec_loadout";
-        class addItemToContainer {};
-        class loadout {};
-        class units_init { postInit = 1; };
-    };
-};
