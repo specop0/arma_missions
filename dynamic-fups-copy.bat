3733aac99842ef749a6a3c388b838c91ca387b31
specop0 <specop0@yahoo.de>
2017-08-12 12:28:47 +0200

    add batch file to copy and pack dynamic fups

---
+++ dynamic-fups-copy.bat
@@ -0,0 +1,28 @@
+set directoriesToCopy=FUPS spec_crates spec_dynamic spec_heli spec_loadout spec_tfar spec_zeus
+set filesToCopy=briefing.sqf description.ext initPlayerLocal.sqf initServer.sqf
+
+set sourceDirectory=dynamic_fups.Altis
+set pboManager="C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe"
+
+for %%s in (
+    dynamic_fups_al.pja310
+    dynamic_fups_celle.mbg_celle2
+    dynamic_fups_cher.Chernarus
+    dynamic_fups_malden.Malden
+    dynamic_fups_pan.Panthera3
+    dynamic_fups_panw.Winthera3
+    dynamic_fups_taki.Takistan
+    dynamic_fups_tanoa.Tanoa
+    ) do (
+    for %%d in (%directoriesToCopy%) do (
+        xcopy "%sourceDirectory%\%%d" "%%s\%%d" /s /e /y
+    )
+    for %%f in (%filesToCopy%) do (
+        xcopy "%sourceDirectory%\%%f" "%%s\%%f" /s /e /y
+    )
+    %pboManager% -pack  "%~dp0\%%s" "%~dp0\%%s.pbo"
+)
+
+%pboManager% -pack "%~dp0\%sourceDirectory%" "%~dp0\%sourceDirectory%.pbo"
+
+pause
